267d0719791f7b90ef0b0c7752b289e205fa3514
wikto <victor.ryzak@ontariotechu.net>
2026-04-13 14:20:47 -0400

    changed website

--- website.py
+++ website.py
@@ -1,14 +1,32 @@
 import gradio as gr
 
-def reverse_text(input_text):
-    return input_text[::-1]
+from ultralytics import YOLOv8
+
+model = YOLOv8.from_pretrained("Perception365/VehicleNet-Y26s")
+source = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+model.predict(source=source, save=True)
+
+
+def predict_image(img, conf_threshold, iou_threshold):
+    results = model.predict(
+        source=img,
+        conf=conf_threshold,
+        iou=iou_threshold,
+        show_labels=True,
+        show_conf=True,
+    )
+    return results[0].plot() if results else None
+
 
 iface = gr.Interface(
-    fn=reverse_text,
-    inputs="text",
-    outputs="text",
-    title="Text Reverser",
-    description="Enter text, and this app will reverse it.",
+    fn=predict_image,
+    inputs=[
+        gr.Image(type="pil", label="Upload Image"),
+        gr.Slider(minimum=0, maximum=1, value=0.25, label="Confidence threshold"),
+        gr.Slider(minimum=0, maximum=1, value=0.45, label="IoU threshold"),
+    ],
+    outputs=gr.Image(type="pil", label="Result"),
+    title="Ultralytics Gradio YOLO26",
+    description="Upload images for YOLO26 object detection.",
 )
-
 iface.launch()
